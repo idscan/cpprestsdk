eff503b98a458f7e9d16fa9cbe16b61c2130a943
john-forrest <john.forrest@gbgplc.com>
2024-08-28 14:19:04 +0100

    Cherry-pick unmerged #1706 from upstream

--- Release/src/http/client/http_client_asio.cpp
+++ Release/src/http/client/http_client_asio.cpp
@@ -30,7 +30,8 @@
 #include <boost/asio/ssl.hpp>
 #include <boost/asio/ssl/error.hpp>
 #include <boost/asio/steady_timer.hpp>
-#include <boost/bind.hpp>
+// Cherry pick https://github.com/microsoft/cpprestsdk/pull/1706
+#include <boost/bind/bind.hpp>
 #if defined(__clang__)
 #pragma clang diagnostic pop
 #endif
